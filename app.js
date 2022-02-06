cbe7a725d2a2d33895365aad5149881b694d604f
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-05 22:39:06 -0500

    add script

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World!</h1>");
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("yes, connected to server...");
